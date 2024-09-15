f85e11ac4b064804783ff77a5eb1e4eb0a67bf5a
MistyIslam5 <mdmisty5@gmail.com>
2024-09-15 23:26:02 +0600

    learning

--- js/event.js
+++ js/event.js
@@ -1,1 +1,22 @@
-console.log('This is a Dom')
+// console.log('This is a Dom')
+// option 2
+function makYellow (){
+    document.body.style.backgroundColor = 'yellow'
+}
+
+// option 3 
+
+const makBlue = document.getElementById('makeBlue');
+makBlue.onclick = makeBlue;
+
+
+function makeBlue (){
+    document.body.style.backgroundColor = 'blue'
+}
+
+// option 3 another option
+
+const makePurple = document.getElementById('makePurple')
+makePurple.onclick = function makePurple (){
+    document.body.style.backgroundColor = 'purple';
+}

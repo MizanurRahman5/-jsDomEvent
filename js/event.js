6b067f714a1b28704b2420889d2695fe8d9e18f3
MistyIslam5 <mdmisty5@gmail.com>
2024-09-16 09:20:59 +0600

    learning

--- js/event.js
+++ js/event.js
@@ -20,3 +20,30 @@ const makePurple = document.getElementById('makePurple')
 makePurple.onclick = function makePurple (){
     document.body.style.backgroundColor = 'purple';
 }
+
+
+const makeGreen = document.getElementById('make-green').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'green';
+})
+
+
+document.getElementById('evnt-listenar').addEventListener('click', function(){
+    // console.log('clicked')
+    const header = document.getElementById('header');
+    header.innerText= 'This is worked';
+})
+
+
+document.getElementById('Update-btn').addEventListener('click', function(){
+    // console.log('clicked')
+    const inputField = document.getElementById('input-text');
+    const inputText = inputField.value
+
+    // console.log(inputText.value)
+    const p = document.getElementById('chang-Text');
+    p.innerText = inputText;
+    inputField.value = ' ';
+
+
+
+})
